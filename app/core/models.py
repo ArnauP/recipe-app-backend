4ceaf8de07cc211ab0b2c74f3103f0cd9ffc0575
aplans <aplans@ingeniamc.com>
2021-12-16 14:48:22 +0100

    Add verification email when creating user

--- app/core/models.py
+++ app/core/models.py
@@ -7,7 +7,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **kwargs):
         """Creates and saves a new user"""
-        user = self.model(email=email, **kwargs)
+        if not email:
+            raise ValueError('Email address is required.')
+        user = self.model(email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,8 +6,8 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email successfully"""
-        email = "test@domain.com"
-        password = "password1234"
+        email = 'test@domain.com'
+        password = 'password1234'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -15,3 +15,15 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@DOMAIN.COM'
+        user = get_user_model().objects.create_user(email, 'pwd123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'pwd123')
